dc7c87f185b78606bff6950b9f28fdef07b2bea3
hardcommitoneself <hardcommitoneself@gmail.com>
2022-02-14 13:12:45 +0700

    Initial Commit

---
+++ SwiftUICalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICalculator
+//
+//  Created by Эдуард Логинов on 14.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUICalculator/SwiftUICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICalculatorApp.swift
+//  SwiftUICalculator
+//
+//  Created by Эдуард Логинов on 14.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
