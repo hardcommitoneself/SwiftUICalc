3ac16fac43bec1017eb43d53c605eb10f8a415b4
hardcommitoneself <hardcommitoneself@gmail.com>
2022-02-16 20:01:13 +0700

    Added some assets -All needed colors, fonts -Extension for fonts and enum for color constants

---
+++ SwiftUICalculator/Constants/Colors.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+enum Colors {
+    static let blackText = Color("blackText")
+    static let shadowBlack = Color.black.opacity(0.1)
+    static let darkBlue = Color("darkBlue")
+    static let shadowBlue = Color("shadowBlue")
+    static let lightGray = Color("lightGray")
+    static let lightGreen = Color("lightGreen")
+    static let darkGreen = Color("darkGreen")
+    static let white = Color.white
+}
---
+++ SwiftUICalculator/Support/Fonts.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+extension Font {
+    static func museo(size: CGFloat = 28) -> Font {
+        return Font.custom("Museo-700", size: size)
+    }
+    
+    static func montserrat(size: CGFloat = 29) -> Font {
+        return Font.custom("Montserrat-Bold", size: size)
+    }
+    
+    static func digital(size: CGFloat = 40) -> Font {
+        return Font.custom("Digital-7Mono", size: size)
+    }
+}
